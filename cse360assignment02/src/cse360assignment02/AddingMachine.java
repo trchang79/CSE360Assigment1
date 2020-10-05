739fed68f20609f1e46daf545399c4f828a8f664
Tyler R Chang <trchang@asu.edu>
2020-10-05 15:31:07 -0700

    First change, edited add and sub to change total, created a history, and clear history.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,33 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String history = "0 ";
+
 
 	public AddingMachine() {
 		total = 0; // not needed - included for clarity
 	}
 
 	public int getTotal() {
-		return 0;
+		return total; // changed to return current total
 	}
 
 	public void add(int value) {
+		total += value; // changed to add value to total
+		history +=  "+ " + value + " ";
 	}
 
 	public void subtract(int value) {
+		total -= value; // changed to subtract value from total
+		history +=  "- " + value + " ";
 	}
 
 	public String toString() {
-		return "";
+		return history;
 	}
 
 	public void clear() {
+		history = "0 ";
 	}
+
 }
